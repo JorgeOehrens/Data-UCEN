b48cb89ec36d8b31a0bb161fcbf0513a762b5f52
Jorge Oehrens <jorge.oehrens@gmail.com>
2021-12-20 18:54:30 -0300

    Update

--- rendimiento/admin.py
+++ rendimiento/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pais
+
+
+admin.site.register(Pais)
 
 # Register your models here.
--- rendimiento/models.py
+++ rendimiento/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CommaSeparatedIntegerField
+from django.utils.timezone import now
 
 # Create your models here.
+
+#Datos de la tabla index
+class Pais(models.Model):
+
+    nombre = models.CharField(max_length=255)
+    
+
+    def __str__(self):
+        return str(self.nombre) 
+
+class Exportador (models.Model):
+    pais = models.ForeignKey(to=Pais.id, on_delete=models.CASCADE, )
+    nombre = models.CharField(max_length=255)
+    rut=models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.nombre) + str(self.rut) + str(self.pais)
+
+
+class Importador (models.Model):
+    pais = models.ForeignKey(to=Pais.id, on_delete=models.CASCADE, )
+    nombre = models.CharField(max_length=255)
+    rut=models.CharField(max_length=255)
+    def __str__(self):
+        return str(self.nombre) + str(self.rut) + str(self.pais)
+
+class Producto ( models.Model):
+    descripcion= models.CharField(max_length=255)
+    precio= models.IntegerField
+
+    def __str__(self):
+        return str(self.descripcion) + str(self.precio)
+
+class Detalle (models.Model):
+    importador = models.ForeignKey(to=Importador.id, on_delete=models.CASCADE)
+    exportador = models.ForeignKey(to=Exportador.id, on_delete=models.CASCADE)
+    producto = models.ForeignKey(to=Producto.id , on_delete= models.CASCADE)
+    cantidad = models.IntegerField
+    tipo_transporte = models.CharField(max_length=255)
+    fecha = models.DateField(default=now)
